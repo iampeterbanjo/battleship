81aa9ea8f644954c282a6db9cfbc7ca2f97ae45b
Peter Banjo <peterbanjo@gmail.com>
2016-01-24 06:43:02 +0000

    it should have a computer

--- scripts/battleship-spec.js
+++ scripts/battleship-spec.js
@@ -1,6 +1,10 @@
-describe("battleship", function() {
-	it("should have a player", function() {
+describe('battleship', function() {
+	it('should have a player', function() {
 		var battleship = new Battleship()
 		expect(battleship.player).toBeDefined()
+	});
+
+	it('should have a computer', function() {
+		expect(new Battleship().computer).toBeDefined()
 	});
 });
